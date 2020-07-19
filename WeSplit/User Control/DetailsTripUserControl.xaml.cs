7615a844a853a643685ed2483d9663cf55b9db51
TỐ NGÔ TẤT <18600014@student.hcmus.edu.vn>
2020-07-19 12:15:56 +0700

    end_TT

--- WeSplit/User Control/DetailsTripUserControl.xaml.cs
+++ WeSplit/User Control/DetailsTripUserControl.xaml.cs
@@ -116,8 +116,9 @@ namespace WeSplit.User_Control
             var queryTrip = from trip in DataProvider.Ins.DB.CHUYENDI
                             where trip.MA_CHUYENDI == MaCD
                             select trip;
-            table_THANHVIEN.ItemsSource = (from tg in DataProvider.Ins.DB.THAMGIA join tv in DataProvider.Ins.DB.THANHVIEN on tg.MATV equals tv.MATV select new { tv.TENTV, tv.SDT, tg.TIENTHU, tg.MACD })
-                                            .Where(tg => tg.MACD == MaCD).ToList();
+            table_THANHVIEN.ItemsSource = (from tg in DataProvider.Ins.DB.THAMGIA 
+                                           join tv in DataProvider.Ins.DB.THANHVIEN on tg.MATV equals tv.MATV
+                                           select new { tv.TENTV, tv.SDT, tg.TIENTHU, tg.MACD }).Where(tg => tg.MACD == MaCD).ToList();
             var selectTrip = (from cd in DataProvider.Ins.DB.CHUYENDI
                               where cd.MA_CHUYENDI == MaCD
                               select cd);
@@ -145,6 +146,13 @@ namespace WeSplit.User_Control
             var sumtc = placeKC.Where(su => su.MA_CHUYENDI == MaCD).ToList();
             var sumKC = sumtc.Select(sum => sum.SOTIENCHI).Sum();
             var SUM = sumAir + sumKS + sumTS + sumKC;
+            //Tính trung bình khoản chi
+            var count = sumtt.Count();
+            var avg = (SUM / count);
+            listCash.ItemsSource = (from tg in DataProvider.Ins.DB.THAMGIA
+                                    join tv in DataProvider.Ins.DB.THANHVIEN on tg.MATV equals tv.MATV 
+                                    where tg.MACD == MaCD
+                                    select new { tv.TENTV, AVG = (int)tg.TIENTHU - (int)avg} ).ToList();  
             sumTC.Text = Convert.ToString(SUM);
             double mul = (double)sumKT - (double)SUM;
             if (mul < 0)
